352dae0907e7007d4ecb5a5c121db82e1a44a7d6
Drakoxw <dragon12xw@gmail.com>
2023-05-28 22:25:38 -0500

    fix: loading

--- src/app/state/app.store.ts
+++ src/app/state/app.store.ts
@@ -1,11 +1,11 @@
-import { InitialStateItems } from "@interfaces/models"; 
-import { ActionReducerMap } from "@ngrx/store";
-import { ItemsReducer } from "./reducers/items.reducer";
+import { InitialStateItems } from '@interfaces/models';
+import { ActionReducerMap } from '@ngrx/store';
+import { ItemsReducer } from './reducers/items.reducer';
 
 export interface AppState {
-    items: InitialStateItems,
+  items: InitialStateItems;
 }
 
 export const ROOT_REDUCER: ActionReducerMap<AppState> = {
-    items: ItemsReducer
-}
+  items: ItemsReducer,
+};
--- src/app/state/selectors/items.selector.ts
+++ src/app/state/selectors/items.selector.ts
@@ -3,7 +3,7 @@ import { InitialStateItems } from '@interfaces/models'
 import { AppState } from '@state/app.store'
 
 export const selectItemsFeature = (state: AppState) => state.items
- 
+
 export const selectListItems = createSelector(
   selectItemsFeature,
   (state: InitialStateItems) => state.itemsList
@@ -11,5 +11,5 @@ export const selectListItems = createSelector(
 
 export const selectLoadingItems = createSelector(
   selectItemsFeature,
-  (state: InitialStateItems) => state.loading
+  (state: InitialStateItems) => state.itemsList.length > 0 ? false : state.loading
 )
